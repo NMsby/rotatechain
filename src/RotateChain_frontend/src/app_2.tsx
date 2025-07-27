16431b2865d6f0b074e55ad3f948938a097bef35
Rogetz <91881364+Rogetz@users.noreply.github.com>
2025-07-27 08:52:21 +0300

    Update app_2.tsx

--- src/RotateChain_frontend/src/app_2.tsx
+++ src/RotateChain_frontend/src/app_2.tsx
@@ -16,75 +16,17 @@ import SassyBurgerMenu from "./hamburgerMenu";
 import JoinGroupPage from "./joinGroup_parameters";
 
 
-// imported from the internet identity file
+
 import { AuthClient } from '@dfinity/auth-client';
 import LoginPage from './loginpage';
 import { UserData } from './types';
 import { Actor } from "@dfinity/agent";
-///I'll use the ledger canister for accountId
+
 import { canisterId as ledgerCanisterId } from "../../declarations/icp_ledger_canister"
 import { canisterId, createActor } from "../../declarations/chain_management";
 import { AccountIdentifier } from "@dfinity/ledger-icp";
 import { Principal } from "@dfinity/principal";
 
-
-
-// this is meant to be the onboarding for anyone joining through the links before they are redirected to the actual groups. See how to incorporate with the email folder that
-// remember the email folder has the logic so transfer some of the logic here and some generation logic to the dashboard,
-//  also remember the security in the initial onboarding gui
-//import JoinGroupPage from "./joinGroup_parameters"
-
-/*function App() {
-  const [activeTab, setActiveTab] = useState('dashboard');
-
-  return (
-    <Layout>
-      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
-      {activeTab === 'dashboard' &&
-       <DeFiDashboard />}
-      {activeTab === 'swap' && <WalletProvider/>}
-      {activeTab === 'pools' && <PoolAnalyticsCard />}
-      {activeTab === 'staking' && <Staking />}
-    </Layout>
-  );
-}
-
-export default App;*/
-
-/*const App = () => {
-  const [activeTab, setActiveTab] = useState<string>('dashboard');
-
-  return (
-    <BrutalLayout>
-      <SassyNav activeTab={activeTab} setActiveTab={setActiveTab} />
-      
-      {activeTab === 'dashboard' && <DeFiDashboard />}
-      {activeTab === 'swap' && <WalletProvider/>}
-      {activeTab === 'pools' && <PoolAnalyticsCard apr={2400} fees24h={25000} tvl={20090} volume24h={23490} />}
-
-      {
-      <div className="mt-20 p-8 text-center">
-        <motion.h1 
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.2 }}
-          className="text-6xl md:text-9xl font-black mb-8"
-        >
-          Rotate.<span className="text-gray-400">Chain</span>
-        </motion.h1>
-        
-        <motion.p
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.4 }}
-          className="max-w-2xl mx-auto text-xl font-medium"
-        >
-          Active Tab: <span className="font-bold">{activeTab.toUpperCase()}</span>
-        </motion.p>
-      </div>}
-    </BrutalLayout>
-  );
-};*/
 
 const roundChain: Chain = {
       id: '0xajs273782bsh372837bdh322ba8',
@@ -339,13 +281,9 @@ function App(){
           <Route path="/splash" element={<SassySplash/>}/>
           <Route path="/rotateTheme" element={<RotateTheme/>}/>
           <Route path="/join" element={<SmartOnboarding setChainData={setChainData}  chainActor={chainActor} authClient={authClient} onLogout={handleLogout} />}/>
-          <Route path="/dashboard" element={<Dashboard chainActor={chainActor} authClient={authClient}  onLogout={handleLogout} roundChain={chainData} />}/>
-          <Route path="/fakeDashboard" element={<DashboardWithLiquidity/>}/>          
+          <Route path="/dashboard" element={<Dashboard chainActor={chainActor} authClient={authClient}  onLogout={handleLogout} roundChain={chainData} />}/>         
           <Route path="/metaDashboard" element={<MetapoolLiquidityDashboard/>}/>
-          <Route path="/loginSection" element={<LoginSection authClient={authClient} />}/>
           <Route path="/login" element={<AppIdentityIntegrated handleLogin={handleLogin} handleLogout={handleLogout} chainActor={chainActor} setChainData={setChainData} />}/>
-          <Route path="/poolAnalytics" element={<LiquidityPoolDashboard/>}/>
-          <Route path="/icp-app" element={<ICPApp  />}/>
           <Route path="/loginPage" element={<LoginPage onLogin={handleLogin} authClient={authClient} />}/>
           <Route path={`/join/:inviteCode`} element={<JoinGroupPage setChainData={setChainData} authClient={authClient}  chainActor={chainActor} />}/>
         </Routes>
