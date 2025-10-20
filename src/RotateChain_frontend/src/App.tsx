9e4876b8116a092787513e313909cf7d4e2ece74
Charlene Mbugua <charlenembugua@Charlenes-MacBook-Pro.local>
2025-10-21 00:56:29 +0300

    resolved naming convention

--- src/RotateChain_frontend/src/App.tsx
+++ src/RotateChain_frontend/src/App.tsx
@@ -43,7 +43,7 @@ function App() {
                   path="/dashboard/*"
                   element={
                     <ProtectedRoute>
-                      <DashboardLayoutWithAI />
+                      <DashboardLayout />
                     </ProtectedRoute>
                   }
                 />
